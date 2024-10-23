d2401a724e83ee35124de0f4e5a60729cb8f3d6e
Fábio Pinto Figueiredo <pf95fabio@gmail.com>
2024-10-22 21:47:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+import {nome, idade, Soma} from "./mensagem.js";
+
+
+console.log(nome)
+console.log(idade)
+
+console.log(Soma(17,26))
---
+++ mensagem.js
@@ -0,0 +1,6 @@
+export const nome = "meu nome"
+export const idade = "minha idade"
+export function Soma(a,b){
+    return a + b
+}
+
